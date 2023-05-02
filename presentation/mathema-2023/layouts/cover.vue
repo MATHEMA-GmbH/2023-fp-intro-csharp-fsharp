13ab5d0202f29df9f022149eca25d712d65916bf
Patrick Drechsler <patrick@pdrechsler.de>
2023-05-03 00:34:00 +0200

    🐛 fix mathema layout (use absolut path)

--- presentation/mathema-2023/layouts/cover.vue
+++ presentation/mathema-2023/layouts/cover.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="slidev-layout cover">
     <div class="flex flex-col justify-between items-start ml-5">
-      <img src="theme/mathema-logo.svg" alt="Mathema Logo" class="w-50 object-fit-contain" />
+      <img src="/theme/mathema-logo.svg" alt="Mathema Logo" class="w-50 object-fit-contain" />
 
       <div class="flex flex-col justify-center items-start ml-5 mt-10">
         <slot />
