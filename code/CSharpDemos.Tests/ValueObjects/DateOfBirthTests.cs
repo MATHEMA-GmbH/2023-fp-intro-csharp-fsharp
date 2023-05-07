f27bcd4ccfe8c39e6de172c8f5937521970cced8
Patrick Drechsler <patrick.drechsler@mathema.de>
2023-05-07 16:31:15 +0200

    ✅ code: fix assertions

--- code/CSharpDemos.Tests/ValueObjects/DateOfBirthTests.cs
+++ code/CSharpDemos.Tests/ValueObjects/DateOfBirthTests.cs
@@ -1,6 +1,6 @@
-using System;
 using CSharpDemos.ValueObjects;
 using FluentAssertions;
+using FluentAssertions.Extensions;
 using Xunit;
 
 namespace CSharpDemos.Tests.ValueObjects
@@ -8,10 +8,10 @@ namespace CSharpDemos.Tests.ValueObjects
     public class DateOfBirthTests
     {
         [Fact]
-        public void Formatting()
-        {
-            new DateOfBirthVO(new DateTime(1900, 12, 31, 23, 59, 59)).ToString()
-                .Should().Be("1900-12-31");
-        }
+        public void Formatting() =>
+            new DateOfBirthVO(31.December(1900).At(23, 59, 59))
+                .ToString()
+                .Should()
+                .Be("1900-12-31");
     }
 }
--- code/CSharpDemos.Tests/WorkflowTests.cs
+++ code/CSharpDemos.Tests/WorkflowTests.cs
@@ -1,6 +1,5 @@
 using FluentAssertions;
 using LaYumba.Functional;
-using System;
 using System.Collections.Immutable;
 using Xunit;
 using static CSharpDemos.Tests.TestHelper.ContactHelper;
@@ -17,10 +16,10 @@ namespace CSharpDemos.Tests
             var emptyAddressBook = new AddressBook(ImmutableList<Contact>.Empty);
             var homer = CreateSampleContact(1, "Homer", "Simpson");
 
-            Func<Contact, Either<string, Contact>> sendMailFunction = c => Right(homer);
+            Either<string, Contact> SendMailFunction(Contact c) => Right(homer);
 
             // Act
-            var result = emptyAddressBook.AddWorkflow(sendMailFunction, homer);
+            var result = emptyAddressBook.AddWorkflow(SendMailFunction, homer);
 
             // Assert
             result.ToString().Should().Be("Right(AddressBook. Number of entries: 1)");
@@ -33,10 +32,10 @@ namespace CSharpDemos.Tests
             var emptyAddressBook = new AddressBook(ImmutableList<Contact>.Empty);
             var homer = CreateSampleContact(1, "Homer", "Simpson");
 
-            Func<Contact, Either<string, Contact>> sendMailFunction = c => Left("ups");
+            Either<string, Contact> SendMailFunction(Contact c) => Left("ups");
 
             // Act
-            var result = emptyAddressBook.AddWorkflow(sendMailFunction, homer);
+            var result = emptyAddressBook.AddWorkflow(SendMailFunction, homer);
 
             // Assert
             result.ToString().Should().Be("Left(ups)");
