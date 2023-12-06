56cb70a0733bd33990c29b8e857488b5703eefb0
Patrick Drechsler <patrick@pdrechsler.de>
2023-12-07 00:07:38 +0100

    ✨ Immutability demo: Add default example

---
+++ code-dotnet8/tests/Examples1CSharpTests/Example01Immutability/PersonTests.cs
@@ -0,0 +1,15 @@
+using Examples1CSharp.Example01Immutability;
+
+namespace Examples1CSharpTests.Example01Immutability;
+
+public class PersonTests
+{
+    [Fact]
+    public void Person1_is_immutable()
+    {
+        var person1 = new Person1("John");
+        person1.Name.Should().Be("John");
+        person1.Name = "Jane";
+        person1.Name.Should().Be("Jane");
+    }
+}
