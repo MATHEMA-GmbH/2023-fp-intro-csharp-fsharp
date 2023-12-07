ecbbc934b62fd44168c2bca10a571e3a89c0a3f1
Patrick Drechsler <patrick@pdrechsler.de>
2023-12-07 03:05:13 +0100

    ✨ Add F# demos (incl smart ctor)

--- code-dotnet8/src/Examples1FSharp/Example01ValueObject/People.fs
+++ code-dotnet8/src/Examples1FSharp/Example01ValueObject/People.fs
@@ -4,3 +4,21 @@ type Person1 = {
     FirstName: string
     LastName: string
 }
+
+type Person2 = private {
+    FirstName: string
+    LastName: string
+}
+
+module Person2 =
+    let isValid (fn: string) (ln: string) =
+        fn.Length > 1 && ln.Length > 1
+        
+    let create (fn: string) (ln: string) =
+        if isValid fn ln then
+            Some { FirstName = fn; LastName = ln }
+        else
+            None
+            
+    let value (p: Person2) = (p.FirstName, p.LastName)
+    let FirstName (p: Person2) = p.FirstName
--- code-dotnet8/tests/Examples1FSharpTests/Example01ValueObject/PeopleTests.fs
+++ code-dotnet8/tests/Examples1FSharpTests/Example01ValueObject/PeopleTests.fs
@@ -1,1 +1,31 @@
 module Examples1FSharpTests.Example01ValueObject.PeopleTests
+
+open Examples1FSharp.Example01ValueObject.People
+open Swensen.Unquote
+open Xunit
+
+[<Fact>]
+let ``Person1 is immutable`` () =
+    let homer =  { Person1.FirstName = "Homer"; LastName = "Simpson"}
+    // homer.FirstName <- "Bart"  // Error: Won't compile
+    true =! true
+
+[<Fact>]
+let ``Person1 is comparable`` () =
+    let homer1 =  { Person1.FirstName = "Homer"; LastName = "Simpson"}
+    let homer2 =  { Person1.FirstName = "Homer"; LastName = "Simpson"}
+    homer1 =! homer2
+    
+[<Fact>]
+let ``Person2 has logic`` () =
+    // test helper method
+    let hasValidNames (input: Person2 option) i =
+        match input with
+        | None -> true =! false // fail, should have been valid
+        | Some x -> Person2.value x =! i // pass, should have been valid
+
+    let validPerson = Person2.create "Homer" "Simpson"
+    hasValidNames validPerson ("Homer", "Simpson")
+    
+    let invalidPerson = Person2.create "Homer" ""
+    invalidPerson =! None
