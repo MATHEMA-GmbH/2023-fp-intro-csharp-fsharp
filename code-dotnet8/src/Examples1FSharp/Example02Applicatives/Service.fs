48f0e48e710288655f6dcc4d9fced9898cc2349e
Martin Grotz <martin.grotz@mathema.de>
2023-12-08 08:26:03 +0100

    create types

--- code-dotnet8/src/Examples1FSharp/Example02Applicatives/Service.fs
+++ code-dotnet8/src/Examples1FSharp/Example02Applicatives/Service.fs
@@ -1,2 +1,41 @@
 module Examples1FSharp.Example02Applicatives.Service
 
+open System
+
+type Errors = String list
+
+type InputDto = {
+    receiverFirstName : String
+    receiverLastName : String
+    receiverTitle : String
+    receiverAddress : String
+    text : String
+}
+
+type NonEmptyString = NonEmptyString of String
+type Kartentext = Kartentext of String
+
+let createNonEmptyString (s : String) : Result<NonEmptyString, Errors> =
+    // TODO: validation
+    Ok (NonEmptyString s)
+
+let createKartentext (s : String) : Result<Kartentext, Errors> =
+    // TODO: validation
+    Ok (Kartentext s)
+
+type Empfaenger = {
+    Vorname : NonEmptyString
+    Nachname : NonEmptyString
+    Postanschrift: NonEmptyString
+    Anrede: NonEmptyString option
+}
+
+type Karte = {
+    Empfaenger : Empfaenger
+    Text : Kartentext
+}
+
+let validate (input : InputDto) : Result<Karte, Errors> = 
+    // intern Kette: Result<Karte, Errors>
+    // dann Fehler extrahieren und zurückgeben
+    Error []
--- code-dotnet8/tests/Examples1FSharpTests/Example02Applicatives/ServiceTests.fs
+++ code-dotnet8/tests/Examples1FSharpTests/Example02Applicatives/ServiceTests.fs
@@ -1,2 +1,21 @@
 module Examples1FSharpTests.Example02Applicatives.ServiceTests
 
+open Examples1FSharp.Example02Applicatives.Service
+open Swensen.Unquote
+open Xunit
+
+[<Fact>]
+let ``validating happy path`` () =
+    let input = {
+        receiverFirstName = "Hans"
+        receiverLastName = "Maulwurf"
+        receiverTitle = "Herr"
+        receiverAddress = "Springfield"
+        text = "Liebe Grüße!"
+    }
+
+    let validated = validate input
+
+    let expectedResult = Error []
+
+    validated =! expectedResult
